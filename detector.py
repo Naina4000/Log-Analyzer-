c3000d495e52846f26e90c4424d7940c45e22d9b
Naina Jain <123048345+Naina4000@users.noreply.github.com>
2026-03-04 18:37:03 +0530

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,1 +1,86 @@
-.
+from collections import defaultdict
+
+
+def detect_bruteforce(parsed_logs, threshold, time_window):
+    """
+    Detect brute force attempts within time window.
+    """
+
+    failed_attempts = defaultdict(list)
+    alerts = []
+
+    # Collect failed login timestamps per IP
+    for log in parsed_logs:
+        if log["status"] == "FAILED":
+            failed_attempts[log["ip"]].append(log["timestamp"])
+
+    # Check time-window logic
+    for ip, timestamps in failed_attempts.items():
+        timestamps.sort()
+
+        for i in range(len(timestamps) - threshold + 1):
+            start_time = timestamps[i]
+            end_time = timestamps[i + threshold - 1]
+
+            if (end_time - start_time).total_seconds() <= time_window:
+                alerts.append({
+                    "type": "Brute Force",
+                    "ip": ip,
+                    "attempts": threshold,
+                    "severity": "HIGH",
+                    "start_time": start_time,
+                    "end_time": end_time,
+                    "duration_seconds": int(
+                        (end_time - start_time).total_seconds()
+                    )
+                })
+                break
+
+    return alerts
+
+
+def detect_username_enumeration(parsed_logs, threshold, time_window):
+    """
+    Detect multiple different usernames attempted from same IP
+    within time window.
+    """
+
+    ip_user_map = defaultdict(list)
+    alerts = []
+
+    # Collect usernames per IP
+    for log in parsed_logs:
+        if log["status"] == "FAILED":
+            ip_user_map[log["ip"]].append(
+                (log["timestamp"], log["username"])
+            )
+
+    for ip, entries in ip_user_map.items():
+        entries.sort()
+
+        for i in range(len(entries)):
+            start_time = entries[i][0]
+            unique_users = set()
+
+            for j in range(i, len(entries)):
+                if (entries[j][0] - start_time).total_seconds() <= time_window:
+                    unique_users.add(entries[j][1])
+
+                    if len(unique_users) >= threshold:
+                        alerts.append({
+                            "type": "Username Enumeration",
+                            "ip": ip,
+                            "attempts": len(unique_users),
+                            "severity": "MEDIUM",
+                            "start_time": start_time,
+                            "end_time": entries[j][0],
+                            "duration_seconds": int(
+                                (entries[j][0] - start_time).total_seconds()
+                            )
+                        })
+                        break
+                else:
+                    break
+
+    return alerts
+
