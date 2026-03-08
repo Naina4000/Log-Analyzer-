e357b42fc50c0186893f517283f9a83507ff67b3
Naina Jain <123048345+Naina4000@users.noreply.github.com>
2026-03-08 15:47:02 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -5,16 +5,15 @@ from datetime import datetime
 def parse_ssh_log(line):
     year = datetime.now().year
 
-    # Failed login pattern
+    # Failed login
     failed_pattern = re.search(
         r"(?P<timestamp>\w+\s+\d+\s+\d+:\d+:\d+).*Failed password for (invalid user )?(?P<user>\w+) from (?P<ip>\d+\.\d+\.\d+\.\d+)",
         line
     )
 
     if failed_pattern:
-        timestamp_str = failed_pattern.group("timestamp")
         timestamp = datetime.strptime(
-            f"{timestamp_str} {year}",
+            f"{failed_pattern.group('timestamp')} {year}",
             "%b %d %H:%M:%S %Y"
         )
 
@@ -22,19 +21,19 @@ def parse_ssh_log(line):
             "timestamp": timestamp,
             "ip": failed_pattern.group("ip"),
             "username": failed_pattern.group("user"),
-            "status": "FAILED"
+            "status": "FAILED",
+            "log_type": "SSH"
         }
 
-    # Successful login pattern
+    # Successful login
     success_pattern = re.search(
         r"(?P<timestamp>\w+\s+\d+\s+\d+:\d+:\d+).*Accepted password for (?P<user>\w+) from (?P<ip>\d+\.\d+\.\d+\.\d+)",
         line
     )
 
     if success_pattern:
-        timestamp_str = success_pattern.group("timestamp")
         timestamp = datetime.strptime(
-            f"{timestamp_str} {year}",
+            f"{success_pattern.group('timestamp')} {year}",
             "%b %d %H:%M:%S %Y"
         )
 
@@ -42,7 +41,32 @@ def parse_ssh_log(line):
             "timestamp": timestamp,
             "ip": success_pattern.group("ip"),
             "username": success_pattern.group("user"),
-            "status": "SUCCESS"
+            "status": "SUCCESS",
+            "log_type": "SSH"
+        }
+
+    return None
+
+
+def parse_apache_log(line):
+    pattern = re.search(
+        r'(?P<ip>\d+\.\d+\.\d+\.\d+) - - \[(?P<timestamp>.*?)\] "(?P<method>\w+) (?P<url>.*?) HTTP/.*" (?P<status>\d+)',
+        line
+    )
+
+    if pattern:
+        timestamp = datetime.strptime(
+            pattern.group("timestamp"),
+            "%d/%b/%Y:%H:%M:%S"
+        )
+
+        return {
+            "timestamp": timestamp,
+            "ip": pattern.group("ip"),
+            "method": pattern.group("method"),
+            "url": pattern.group("url"),
+            "status_code": int(pattern.group("status")),
+            "log_type": "APACHE"
         }
 
     return None
